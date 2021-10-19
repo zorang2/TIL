d4d414b4428d106a40b1e7e3a5ac917b713beae6
Hyeon Geun, CHO <gusrms3103@gmail.com>
2021-10-19 15:34:21 +0900

    Create Main.java

---
+++ Study_Yeungnam_Univ/JAVA Programming and practice/21.10.19 practice/ex.6.3./Main.java
@@ -0,0 +1,52 @@
+class Main{
+  public static void main(String[] args) {
+    Game game = new Game();
+    game.run();
+  }
+}
+/*
+객관식 이론문제 상속이란무엇인가 이렇게..나옴
+
+단답형도있고
+코드는 빈칸으로 만들어놓고 넣어라 이런식.
+
+문제를만들다보면 한코드에 문제가여러개있다.
+빈칸이 한문제에 여러개.
+
+문제수는 시험시간보다 훨신만타
+
+40분시험이면 60개 이렇게.
+
+구글미트로 들어와야한다.
+
+시험치는과정이 녹화가됨.
+
+빢빢하게 풀도록 공정하게 시험시간보다많이낸다.
+*/
+
+
+/* 내코드
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    
+    //int (Math.random() * 3 + 1);
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("철수[가위(1), 바위(2), 보(3), 끝내기(4)] >> ");
+    int number = scanner.nextInt();
+
+    switch(number){
+      case 1:
+        int a = (int)(Math.random() * 3) + 1);
+      
+      if(a==1)
+        System.out.print
+
+      System.out.print("철수(가위) : ");
+    }
+    
+  }
+}
+*/

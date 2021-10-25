9273a8d0c3d3985280c2227c317a0f304fad5d2e
ZORANG2 <gusrms3103@gmail.com>
2021-10-25 16:22:04 +0900

    middle testing

--- Study_Yeungnam_Univ/Data structure/MiddleTest/eg.1.sc.c
+++ Study_Yeungnam_Univ/Data structure/MiddleTest/eg.1.sc.c
@@ -5,7 +5,7 @@
 int main(){
     int a = 10;
     int *p1 = &a;
-    int *p2 = &p1;
+    int** p2 = &p1;
 
     printf("%p\n", &a);
     printf("%p\n", p1);
--- Study_Yeungnam_Univ/Data structure/MiddleTest/eg4.c
+++ Study_Yeungnam_Univ/Data structure/MiddleTest/eg4.c
@@ -2,12 +2,11 @@
 #include <stdlib.h>
 #include <string.h>
 int doWork(int num) {
-    if (num == 1) return 1;
-    return num * doWork(num-1);
+    if (num == 1 || num == 0) return 1;
+    return num * doWork(num-2);
 }
 
 int main() {
-    int result = 0;
-    int num = 5;
+    int num = 6;
     printf("result : %d", doWork(num));
 }

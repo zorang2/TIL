496d21b18ef57e2008e6a9d2b84803a17b89152f
Hyeon Geun, CHO <gusrms3103@gmail.com>
2021-11-02 14:41:55 +0900

    Update Main.java

--- Study_Yeungnam_Univ/JAVA Programming and practice/21.11.02 practice/ex.7.2/Main.java
+++ Study_Yeungnam_Univ/JAVA Programming and practice/21.11.02 practice/ex.7.2/Main.java
@@ -1,41 +1,6 @@
-//import java.util.Scanner;
-//import java.util.Vector;
-//import java.util.Iterator;
-
-import java.util.*;
-
 class Main {
   public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-
-    //Vector<Integer> v = new Vector<Integer>(); //이렇게 쓰는게 귀찮아서 아래처럼쓴다.
-    var v = new Vector<Integer>(); // variable은 초기화 하면서 써야됨. var v; 이런식으로는 못씀.
-
-    while(true){ // 내가 0입력하기전까진 무한반복해야하니까 while
-      System.out.print("강수량 입력 (0 입력시 종료)>>");
-      int n = scanner.nextInt(); //정수값아닐경우를 대비해 원래라면 try catch문 넣어야된다.
-      if(n == 0) break; // 0입력시 종료되는문. while밖으로 나감
-
-      v.add(n);
-      print(v);
-
-    }
+    LocationManager lm = new LocationManager();
+    lm.run();
   }
-
-  public static void print(Vector<Integer> v) {
-    int sum = 0;
-
-    //Iterator<Integer> = v.iterator(); //이렇게쓰기 귀찮아서 아래처럼 쓴다.
-    var it = v.iterator();
-
-    while(it.hasNext()) {
-      int n = it.next();
-      System.out.print(n + " ");
-      sum += n;
-    }
-
-    System.out.println();
-    System.out.println("현재 평균 " + (sum/v.size()));
-  }
-
 }

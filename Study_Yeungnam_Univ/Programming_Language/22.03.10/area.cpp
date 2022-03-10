5f36e06513505d26766777603992f52955e58a70
Hyeon Geun, CHO <gusrms3103@gmail.com>
2022-03-10 10:45:36 +0900

    Create area.cpp

---
+++ Study_Yeungnam_Univ/Programming_Language/22.03.10/area.cpp
@@ -0,0 +1,20 @@
+#if 0
+#include <iostream>
+
+double area(int r);
+
+double area(int r) {
+	return 3.14 * r * r;
+}
+
+int main() {
+
+	using namespace std;
+
+	int n = 3;
+	char c = '#';
+	cout << c << 5.5 << '-' << n << "hello" << true << endl;
+	cout << "n + 5 = " << n + 5 << "\n";
+	cout << "면적은 " << area(n);
+}
+#endif

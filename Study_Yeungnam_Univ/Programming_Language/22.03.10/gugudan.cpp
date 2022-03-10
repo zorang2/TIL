ffb092fe92eeb574052a56c18a745336a74f977a
Hyeon Geun, CHO <gusrms3103@gmail.com>
2022-03-10 10:45:56 +0900

    Add files via upload

---
+++ Study_Yeungnam_Univ/Programming_Language/22.03.10/gugudan.cpp
@@ -0,0 +1,16 @@
+#if 1
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+
+	for (int i = 1; i <= 9; i++) {
+		for (int j = 1; j <= 9; j++) {
+			cout << i * j << " ";
+		}
+		cout << "\n";
+	}
+}
+
+#endif

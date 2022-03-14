90e0c8e60d01910f266cb3a6fad774e58920754b
Hyeon Geun, CHO <gusrms3103@gmail.com>
2022-03-14 17:04:57 +0900

    Create example3-2.cpp

---
+++ Study_Yeungnam_Univ/Programming_Language/22.03.14/example3-2.cpp
@@ -0,0 +1,27 @@
+/*
+* 예제 3-2 실습
+*/
+
+#if 0
+#include <iostream>
+using namespace std;
+
+class Rectangle {
+public:
+	int width;
+	int height;
+	int getArea();
+};
+
+int Rectangle::getArea() {
+	return width * height;
+}
+
+int main() {
+	Rectangle rect;
+	rect.width = 3;
+	rect.height = 5;
+	cout << "사각형의 면적은 " << rect.getArea() << endl;
+}
+
+#endif

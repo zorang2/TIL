e2297fbbb13561cc8f3a07a198aa3ac68e85c898
Hyeon Geun, CHO <gusrms3103@gmail.com>
2022-10-14 07:23:30 +0000

    [이코테] 2. 그리디 & 구현 (상하좌우까지)

---
+++ Algorithm/python_for_coding_test/2. 그리디 & 구현/거스름돈_내풀이.py
@@ -0,0 +1,34 @@
+n = int(input())
+coin_count = 0
+
+while True:
+    #500
+    result = n // 500
+    exchange = n - (result * 500)
+    coin_count += result
+
+    #100
+    result = exchange // 100
+    exchange = exchange - (result * 100)
+    coin_count += result
+    
+    #50
+    result = exchange // 50
+    exchange = exchange - (result * 50)
+    coin_count += result
+  
+    #10
+    result = exchange // 10
+    exchange = exchange - (result * 10)
+    coin_count += result
+    
+
+    if exchange == 0:
+        break
+
+
+    if exchange < 10:
+        print("10원 단위로 다시 입력하세요.")
+        break
+
+print(coin_count)

44af4da8c7849ca3289823c20b6be0cd77430513
zorang2 <gusrms3103@gmail.com>
2023-04-21 16:50:44 +0900

    04.21 16:49

--- Internship/MobileNetV2_FER/visualizing_copy.py
+++ Internship/MobileNetV2_FER/visualizing_copy.py
@@ -8,10 +8,14 @@ import glob
 IMG_PATH = "/home/sldev1/Desktop/fer/affectnet_full/Manually_Annotated_Images/"
 LABEL_PATH = "/home/sldev1/Desktop/fer/affectnet_full/Manually_Annotated_file_lists/"
 
+f = open(LABEL_PATH + "training.csv")
+lines = f.readline()
+print(lines)
 
-df = pd.read_csv(LABEL_PATH + "validation.csv")
-print(df.drop([1], axis=0))
-# print(df.iloc[:,[0]])
+
+# df = pd.read_csv(LABEL_PATH + "validation.csv")
+# print(df.drop([0], axis=0))
+# print(df.iloc[:,[0,6]])
 
 
 

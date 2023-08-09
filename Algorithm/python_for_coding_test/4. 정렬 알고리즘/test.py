3d65aa4505c9a0b5e64ba96fa0da4df057be6977
zorang2 <gusrms3103@gmail.com>
2023-08-09 16:26:26 +0900

    08.09 sorting

--- Algorithm/python_for_coding_test/4. 정렬 알고리즘/test.py
+++ Algorithm/python_for_coding_test/4. 정렬 알고리즘/test.py
@@ -1,1 +1,66 @@
-print('hellow')
+
+# 퀵 정렬 ( 평균 O(NlogN), 최악 O(N^2) )
+
+array = [5, 7, 9, 0, 3, 1, 6, 2, 4, 8]
+
+def quick_sort(array, start, end):
+    if start >= end: # 원소가 1개인 경우 종료
+        return
+    pivot = start # 피벗은 첫 번째 원소
+    left = start + 1
+    right = end
+    while(left <= right):
+        # 피벗보다 큰 데이터를 찾을 때 까지 반복
+        while(left <= end and array[left] <= array[pivot]):
+            left += 1
+        # 피벗보다 작은 데이터를 찾을 때 까지 반복
+        while(right > start and array[right] >= array[pivot]):
+            right -= 1
+        if(left > right): # 엇갈렸다면 작은 데이터와 피벗을 교체
+            array[right], array[pivot] = array[pivot], array[right]
+        else:
+            array[left], array[right] = array[right], array[left]
+    # 분할 이후 왼쪽 부분과 오른쪽 부분에서 각각 정렬 수행
+    quick_sort(array, start, right-1)
+    quick_sort(array, right + 1, end)
+
+quick_sort(array, 0, len(array)-1)
+print(array)
+
+
+
+
+
+
+
+'''
+# 삽입정렬( O(N^2) )
+# 특징 : 선택 정렬보다 일반적으로 효율적 동작
+#
+
+array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+
+for i in range(1, len(array)):
+    for j in range(i, 0, -1):
+        if array[j] < array[j-1]:
+            array[j], array[j-1] = array[j-1], array[j]
+        else:
+            break
+
+print(array)
+'''
+
+'''
+# 선택정렬( O(N^2) )
+
+array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+
+for i in range(len(array)):
+    min_index = i 
+    for j in range(i+1, len(array)):
+        if array[min_index] > array[j]:
+            min_index = j
+    array[i], array[min_index] = array[min_index], array[i] #스와핑
+
+print(array)
+'''
